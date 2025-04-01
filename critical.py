5194ab1fb994ca7cc64a3d8f050d189d69edcf31
Sty-ven <oriyomistephen@gmail.com>
2025-04-01 20:58:07 +0200

    inserts all the files for the 12 months

--- critical.py
+++ critical.py
@@ -2,12 +2,10 @@ import wget
 import pyarrow.parquet as pq
 import pandas as pd
 from sqlalchemy import create_engine
-
-url = "https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_2024-01.parquet"
-taxi = wget.download(url)
+from time import time
 
 empty_df = []
-for month in range(1, 13):
+for month in range(1, 2):
     url = "https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_2024-{:02d}.parquet".format(
         month
     )
@@ -16,8 +14,25 @@ for month in range(1, 13):
     df = df.to_pandas()  # converts pyarrow table to pandas dataframe
     empty_df.append(df)
 final_df = pd.concat(empty_df)
-subset_df = final_df.iloc[:10000, :]
+print(final_df.head(5))
+final_df = final_df.to_csv("taxi.csv", index=False)
 
-con = create_engine("postgresql://admin:admin@sample_postgres/silent")
+# read csv file
+df_iter = pd.read_csv("taxi.csv", chunksize=100000)
 
-subset_df.to_sql("trip-data", con, if_exists="replace")
+
+while True:
+    try:
+        df = next(df_iter)
+        con = create_engine("postgresql://admin:admin@sample_postgres/silent")
+
+        start_time = time()
+        df.to_sql("trip-data", con, if_exists="append", index=False)
+        end_time = time()
+
+        elapsed_time = end_time - start_time
+        print(f"100000 rows was inserted in {elapsed_time} seconds")
+    except Exception:
+        print("The insertion was unsuccessful")
+
+
