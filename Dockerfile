f98369810c3363a84de6156d5796e740973097d4
Eben Fiifi <ebenfiifi96@gmail.com>
2025-05-10 21:31:58 +0200

    airflow commit

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM apache/airflow:2.10.0
+FROM apache/airflow:2.10.5
 
 #use root to install package
 USER root
@@ -19,7 +19,7 @@ COPY requirements.txt .
 USER airflow
 
 #install python dependencies using pip since that's what you're using
-RUN pip --no-cache-dir -r requirements.txt
+RUN pip install --no-cache-dir -r requirements.txt
 
 #Copy DAGs and other necesary files
 COPY dags/ /opt/airflow/dags
--- dags/critical.py
+++ dags/critical.py
@@ -7,7 +7,7 @@ from sqlalchemy import create_engine
 
 from _datetime import datetime
 
-from airflow import DAG
+from airflow.models import DAG
 from airflow.operators.python import PythonOperator
 
 
@@ -15,9 +15,9 @@ dag = DAG(
  dag_id="my_dag_name",
  start_date=datetime(2025, 4, 1),
  schedule="@daily",
+ catchup= False
 )
 
-@dag
 def download_write_data():
     empty_df = []
     for month in range(1, 13):
@@ -25,33 +25,44 @@ def download_write_data():
             month
         )
         table = wget.download(url)  # downloads the parquet file
-        df = pq.read_table(table)#reads parquet file
+        df = pq.read_table(table)#reads parquet filedf_iter = pd.read_csv("../taxi.csv", chunksize=100000)
         _dff = df.to_pandas() # converts pyarrow table to pandas dataframe
         empty_df.append(_dff)
     final_df = pd.concat(empty_df)
-    # print(final_df.head())
+    print(final_df.head())
     final_df.to_csv("taxi.csv", index=False) #put data into a csv file
     #read csv ile
 
-@dag
-def send_to_database():
-    df_iter = pd.read_csv("../taxi.csv", chunksize=100000)
-    while True:
-        try:
-            con = create_engine("postgresql://admin:admin@sample_postgres/silent")
-            con.connect()
-            df=next(df_iter)
-            start_time = time()
-            df.to_sql("trip_data", con=con, if_exists="append")
-            end_time = time()
-            elapsed_time=start_time-end_time
-            print(f"100000 rows was print as in{elapsed_time} seconds")
-        except Exception:
-            print("The insertion was unsuccessful")
+# def send_to_database():
+#     con = create_engine("postgresql://admin:admin@sample_postgres/silent")
+#     con.connect()
+#     df_iter = pd.read_csv("../taxi.csv", chunksize=100000)
+#
+#     i=0
+#     for part in enumerate(df_iter):
+#         i=+1
+#         start_time = time()
+#         part.to_sql("trip_data", con=con, if_exists="append",index=False)
+#         end_time = time()
+#         elapsed_time=start_time-end_time
+#         print(f"Chunk {i+1} was inserted in {elapsed_time, 2} seconds")
 
-with dag:
-    DOWNLOAD_WRITE_DATA=download_write_data()
-
-    SEND_TO_DATABASE=send_to_database()
-
-DOWNLOAD_WRITE_DATA>>SEND_TO_DATABASE
+# with dag:
+#     DOWNLOAD_WRITE_DATA=download_write_data()
+#
+#     SEND_TO_DATABASE=send_to_database()
+#
+# DOWNLOAD_WRITE_DATA>>SEND_TO_DATABASE
+download_task = PythonOperator(
+    task_id="download_write_data",
+    python_callable=download_write_data,
+    dag=dag
+)
+#
+# send_task = PythonOperator(
+#     task_id="send_to_database",
+#     python_callable=send_to_database,
+#     dag=dag,
+# )
+#
+download_task
